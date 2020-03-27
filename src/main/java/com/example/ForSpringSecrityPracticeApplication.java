e3020c3e22f31a72c8ae75b70c152f8cce258a39
SuzukiKumpei <suzuki.kumpei@gmail.com>
2020-03-27 14:02:25 +0900

    first commit

---
+++ src/main/java/com/example/ForSpringSecrityPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForSpringSecrityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForSpringSecrityPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ForSpringSecrityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForSpringSecrityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
